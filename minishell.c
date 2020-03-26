21fbbc54971c1cce39cac4a31a3affc5fa243103
AmineBarboura <abarbour@DESKTOP-L4TOQQP.localdomain>
2020-03-26 12:53:30 +0100

    petite modification

--- minishell.c
+++ minishell.c
@@ -3,16 +3,18 @@
 int	main(int argc, char **argv, char **envp)
 {
 	char	*line;
+	int		ret;
 
 	argc = argc;
 	argv =argv ;
 	envp =envp ;
 	line = NULL;
 	write(1,"kembyalet$",11);
+	write(1, argv[1], 3);
 	while(1)
 	{
-		get_next_line(0, &line);
-		if(line)
+		ret = get_next_line(0, &line);
+		if(line && ret > 0)
 		{
 			if(!ft_strncmp(line, "exit", 5))
 			{
@@ -21,6 +23,11 @@ int	main(int argc, char **argv, char **envp)
 			}
 			write(1,"kembyalet$",11);
 			free(line);
+		}
+		else
+		{
+			write(1, "\n", 1);
+			break;
 		}
 	}
 	return (0);
