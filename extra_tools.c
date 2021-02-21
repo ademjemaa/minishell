01f38cfea570a5bed39d0a5115f8de7120ec2caa
adjemaa <ademlolplayer@gmail.com>
2021-02-21 18:05:32 +0100

    need to fix special case of endless spaces

--- extra_tools.c
+++ extra_tools.c
@@ -12,18 +12,48 @@
 
 #include "minishell.h"
 
+int		check_end_space(t_cmd *stru, int i)
+{
+	if (stru->args[i][ft_strlen(stru->args[i]) - 2] != '\\')
+	{
+		while (i < stru->total && stru->args[i])
+		{
+			if (stru->args[i][0] != 0)
+				break;
+			i++;
+		}
+		printf("i == %d total == %d, args == %s\n\n", i, stru->total, stru->args[i]);
+		if (i == stru->total)
+			return (1);
+	}
+	return (0);
+}
+
 void	find_spaces(t_cmd *stru)
 {
 	int i;
 	int j;
 
-	i = -1;
-	while (++i < stru->total)
+	i = 0;
+	while (stru->args && stru->args[i] && i < stru->total)
 	{
 		j = 0;
 		while (stru->args[i][j] == ' ')
 			j++;
 		if (stru->args[i][j] == 0)
 			ft_bzero(stru->args[i], ft_strlen(stru->args[i]));
+		i++;
 	}
+/*	i = 0;
+	while (stru->args && stru->args[i] && i < stru->total)
+	{
+		if (stru->args[i][ft_strlen(stru->args[i]) - 1] == ' ')
+			if (check_end_space(stru, i))
+			{
+				printf("allahou\n");
+				stru->args[i][ft_strlen(stru->args[i]) - 1] = 0;
+				printf("akbar\n");
+			}
+		i++;
+	}*/
 }
