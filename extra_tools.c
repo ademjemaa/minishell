fbbe97fe3d0de1bbfeee3b5d76778799eac5ede4
Adam JEMAA <adjemaa@e2r10p2.42.fr>
2021-02-22 13:28:35 +0100

    removed some code

--- extra_tools.c
+++ extra_tools.c
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tools_for_tools.c                                  :+:      :+:    :+:   */
+/*   extra_tools.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adjemaa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 15:20:26 by adjemaa           #+#    #+#             */
-/*   Updated: 2021/02/21 15:55:33 by adjemaa          ###   ########.fr       */
+/*   Updated: 2021/02/22 13:27:41 by adjemaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-int		check_end_space(t_cmd *stru, int i)
-{
-	if (stru->args[i][ft_strlen(stru->args[i]) - 2] != '\\')
-	{
-		while (i < stru->total && stru->args[i])
-		{
-			if (stru->args[i][0] != 0)
-				break;
-			i++;
-		}
-		printf("i == %d total == %d, args == %s\n\n", i, stru->total, stru->args[i]);
-		if (i == stru->total)
-			return (1);
-	}
-	return (0);
-}
 
 void	find_spaces(t_cmd *stru)
 {
@@ -44,16 +27,4 @@ void	find_spaces(t_cmd *stru)
 			ft_bzero(stru->args[i], ft_strlen(stru->args[i]));
 		i++;
 	}
-/*	i = 0;
-	while (stru->args && stru->args[i] && i < stru->total)
-	{
-		if (stru->args[i][ft_strlen(stru->args[i]) - 1] == ' ')
-			if (check_end_space(stru, i))
-			{
-				printf("allahou\n");
-				stru->args[i][ft_strlen(stru->args[i]) - 1] = 0;
-				printf("akbar\n");
-			}
-		i++;
-	}*/
 }
